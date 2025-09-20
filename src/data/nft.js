9222340c34890ff451bc3a467f0e00333c11cee5
Barklegne Eshetu <barklegne.eshetu@gmail.com>
2025-09-19 23:58:23 -0400

    feat: pricing update 4.2

--- src/data/nft.js
+++ src/data/nft.js
@@ -354,7 +354,7 @@ export const addNewNFT = (newNFT) => {
 		owner: {
 			name: 'New Creator',
 			address: '0x1234...5678',
-			avatar: '/avatar4.jpg',
+			avatar: '/avatar5.jpg',
 		},
 		// Add price or auction fields based on the sale type
 		...(newNFT.saleType === 'Buy Now'
